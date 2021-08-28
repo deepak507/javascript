dde21bab0e19861d64201e416c0cdbef13803bf5
deepak507 <deepakbansaljjee@gmail.com>
2021-08-28 10:08:03 +0530

    javascript

---
+++ app.js
@@ -0,0 +1,44 @@
+// console.log("hello world");
+
+// var a = prompt("what is your age");
+// var b = prompt("which year you born");
+// alert(b-a);
+
+// function xyz(one, two){
+//    console.log(one + two);
+
+// }
+
+// xyz(2, 3);
+// function abc (one, two, three){
+//     console.log(one + two - three)
+// }
+// abc(10,2,2);
+
+/* typeof property
+index,length,how to change the index value */
+
+// var name = {"45" , "46" , "47" , "48"}
+// a[0]
+
+
+// function Merge(parm1, parm2) {
+//     var a = parm1 + " love " + parm2
+//     return a;
+// }
+
+// var xyz = Merge("I", "coding");
+// console.log(xyz);
+
+// function abc() {
+//     console.log(2 + 2);
+// }
+
+// abc();
+function deepak(parm1, parm2) {
+    var b = parm1 + " love " + parm2;
+    return b;
+}
+deepak("I", "coding");
+
+console.log(deepak);
